c8d2451c26173331df0b2f4d7a1f63458c99729b
Rob Foulkrod <rob@foulkrodTraining.com>
2020-10-19 15:30:41 -0400

    Added version to route

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace phoenix
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}{version?}");
             });
         }
     }
